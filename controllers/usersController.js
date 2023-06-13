27cb9a2dda981ca86e61ea4ca44b9ed64a6332ee
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-06-13 12:38:31 -0300

    refactor: added auth error message

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -77,7 +77,7 @@ exports.verifyToken = (req, res, next) => {
     const token = req.headers.authorization;
     if (token) {
         jwt.verify(token, process.env.JWT_SECRET, (err) => {
-            if (err) return res.sendStatus(403);
+            if (err) return res.status(403).json({message: "User not authenticated."});
             next();
         });
     } else {
